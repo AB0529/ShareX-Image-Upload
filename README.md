6d27289e51b7df3337761f4d97748e79b315f044
Moist <supercrab115@gmail.com>
2019-04-15 12:45:39 -0400

    Prettied things up.

--- README.md
+++
@@ -1,5 +0,0 @@
-**Install**
-`npm i`
-
-**Usage**
-Add a custom image uploader to ShareX with your client secret
--- server.js
+++ server.js
@@ -5,21 +5,24 @@ const fs = require('fs');
 
 const config = require('./config.json');
 
+// Initialize
 const app = express();
-const port = config.port;
+const port = 3e3;
 const upload = multer();
 
+// Routs
 app.get('/api', (req, res) => {
-  return res.send(`Working`);
+  return res.back(`localhost`);
 });
 
-app.post('/api/upload', upload.single('d'), (req, res) => {
-  if (req.body.key !== config.secret)
+app.post(`/api/upload`, upload.single('d'), (req, res) => {
+  let key = req.body.key;
+  let name = req.body.name;
+
+  // Check secret
+  if (key !== config.secret)
     return res.end(`Unauthorized.`);
-
-  fs.writeFileSync(`./Images/${req.body.name}.png`, req.file.buffer);
-  return res.end(`http://localhost:${port}/Images/${req.body.name}.png`);
-
+  // Upload file
+  fs.writeFileSync(`${config.path}/${name}.png`, req.file.buffer);
+  return res.end(`${config.url}/${name}.png`);
 });
-
-app.listen(port, () => console.log(`App listening on port ${port}`));
